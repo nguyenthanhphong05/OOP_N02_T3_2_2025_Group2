3dedad4b3c0f44348319e6a5c94f493b21ce4032
Đặng Đức Kiên <24100323@st.phenikaa-uni.edu.vn>
2025-08-04 09:06:59 +0000

    update

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+}
+}
